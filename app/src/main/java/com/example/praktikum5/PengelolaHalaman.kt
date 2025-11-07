d0e352c51bb0dd28bdde29908d18d58970673769
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-07 17:56:20 +0700

    buat struktur dasar composable PendaftaranApp dengan parameter navController dan modifier

--- app/src/main/java/com/example/praktikum5/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikum5/PengelolaHalaman.kt
@@ -1,8 +1,24 @@
 package com.example.praktikum5.ui.theme
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+
 // Enum untuk menentukan rute navigasi
 enum class Halaman {
     Beranda,
     List,
     Formulir
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PendaftaranApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+
 }

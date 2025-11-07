6170589ff03820266f9e6d5fc6f2a636ecab25d9
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-07 17:57:50 +0700

    tambahkan Scaffold sebagai container utama aplikasi

--- app/src/main/java/com/example/praktikum5/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikum5/PengelolaHalaman.kt
@@ -1,11 +1,11 @@
 package com.example.praktikum5.ui.theme
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-
 
 // Enum untuk menentukan rute navigasi
 enum class Halaman {
@@ -20,5 +20,9 @@ fun PendaftaranApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
 ) {
+    Scaffold(
+        modifier = modifier
+    ) { innerPadding ->
 
+    }
 }

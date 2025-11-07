3d1b0f35948c4d2d91388baff7ec846e0c9b79b8
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-07 15:52:49 +0700

    atur layout utama dengan Column dan background putih

--- app/src/main/java/com/example/praktikum5/ui/theme/HalamanBeranda.kt
+++ app/src/main/java/com/example/praktikum5/ui/theme/HalamanBeranda.kt
@@ -1,13 +1,31 @@
 package com.example.praktikum5.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import com.example.praktikum5.R
 
 @Composable
 fun HalamanBeranda(
     onNextButtonClicked: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            // Background halaman jadi putih
+            .background(Color.White)
+            .padding(dimensionResource(R.dimen.padding_medium)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
 
+    }
 }

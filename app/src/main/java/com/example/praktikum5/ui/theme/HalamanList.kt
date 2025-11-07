0e109d4cdedf26f20171b623342eb07e2f6742ea
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-07 16:09:04 +0700

    tambahkan struktur dasar composable HalamanList

---
+++ app/src/main/java/com/example/praktikum5/ui/theme/HalamanList.kt
@@ -0,0 +1,13 @@
+package com.example.praktikum5.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HalamanList(
+    onBerandaClicked: () -> Unit,
+    onFormulirClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}

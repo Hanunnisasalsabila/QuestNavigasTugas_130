b6cbde7c91b5ec50254d0b9ae684c7319b2cb0dd
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-07 16:11:52 +0700

    atur layout utama dengan Column dan background ungu

--- app/src/main/java/com/example/praktikum5/ui/theme/HalamanList.kt
+++ app/src/main/java/com/example/praktikum5/ui/theme/HalamanList.kt
@@ -1,7 +1,14 @@
 package com.example.praktikum5.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.example.praktikum5.R
 
 @Composable
 fun HalamanList(
@@ -9,5 +16,13 @@ fun HalamanList(
     onFormulirClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(purpleBackground)
+            .padding(dimensionResource(R.dimen.padding_small)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+    }
 }

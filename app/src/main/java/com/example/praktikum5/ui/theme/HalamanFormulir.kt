d99ffc12be7dd40fd5ce3a2af8fd95247cd922bd
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-07 17:11:44 +0700

    menambahkan struktur dasar project dan file HalamanFormulir.kt

---
+++ app/src/main/java/com/example/praktikum5/ui/theme/HalamanFormulir.kt
@@ -0,0 +1,12 @@
+package com.example.praktikum5.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HalamanFormulir(
+    onFormSubmitClicked: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}

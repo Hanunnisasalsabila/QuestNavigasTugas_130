cd3c34dec76f06699dc548c8cb6fff267da0343c
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-07 17:17:33 +0700

    tambahkan state input pengguna untuk nama, alamat, jenis kelamin, dan status

--- app/src/main/java/com/example/praktikum5/ui/theme/HalamanFormulir.kt
+++ app/src/main/java/com/example/praktikum5/ui/theme/HalamanFormulir.kt
@@ -1,6 +1,10 @@
 package com.example.praktikum5.ui.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -8,5 +12,9 @@ fun HalamanFormulir(
     onFormSubmitClicked: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
 
 }

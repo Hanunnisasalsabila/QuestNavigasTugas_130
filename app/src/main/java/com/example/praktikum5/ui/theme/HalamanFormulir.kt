579ac93ba42afe52f800ed5ba07c0c4b12bd742c
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-07 17:19:34 +0700

    buat layout dasar menggunakan Column dengan background dari Color.kt

--- app/src/main/java/com/example/praktikum5/ui/theme/HalamanFormulir.kt
+++ app/src/main/java/com/example/praktikum5/ui/theme/HalamanFormulir.kt
@@ -1,11 +1,19 @@
 package com.example.praktikum5.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.example.praktikum5.R
 
 @Composable
 fun HalamanFormulir(
@@ -17,4 +25,14 @@ fun HalamanFormulir(
     var textJK by remember { mutableStateOf("") }
     var textStatus by remember { mutableStateOf("") }
 
+    Column(
+        modifier = modifier
+            .background(purpleBackground) // Ambil dari Color.kt
+            .padding(dimensionResource(R.dimen.padding_small))
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
 }

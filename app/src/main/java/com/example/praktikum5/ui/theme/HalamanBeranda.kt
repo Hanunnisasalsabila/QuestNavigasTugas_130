cd73646155f84e573b6c83a14aa92b1eae99a28c
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-07 16:05:00 +0700

    tambahkan tombol Submit dengan warna dari Color.kt

--- app/src/main/java/com/example/praktikum5/ui/theme/HalamanBeranda.kt
+++ app/src/main/java/com/example/praktikum5/ui/theme/HalamanBeranda.kt
@@ -5,8 +5,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,7 +23,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.material3.Text
 import com.example.praktikum5.R
 
 @Composable
@@ -69,6 +74,21 @@ fun HalamanBeranda(
                 color = Color.DarkGray
             )
         }
-
+        // Tombol Submit
+        Button(
+            onClick = onNextButtonClicked,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = purpleButton), // Warna dari Color.kt
+            shape = RoundedCornerShape(size = 25.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.submit),
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
     }
 }

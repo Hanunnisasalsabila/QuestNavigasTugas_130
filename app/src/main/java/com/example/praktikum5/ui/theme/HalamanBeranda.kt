e6a612b21be7db4e396bf23e51dab3b6669c334c
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-07 15:49:01 +0700

    tambahkan struktur dasar composable HalamanBeranda

--- app/src/main/java/com/example/praktikum5/ui/theme/HalamanBeranda.kt
+++ app/src/main/java/com/example/praktikum5/ui/theme/HalamanBeranda.kt
@@ -1,1 +1,13 @@
 package com.example.praktikum5.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun HalamanBeranda(
+    onNextButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}

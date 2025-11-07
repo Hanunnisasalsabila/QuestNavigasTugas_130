1a1c6c47aa50c25879c22d86870e86a345ad4fce
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-07 18:03:10 +0700

    ubah MainActivity.kt agar bisa Run app

--- app/src/main/java/com/example/praktikum5/MainActivity.kt
+++ app/src/main/java/com/example/praktikum5/MainActivity.kt
@@ -7,41 +7,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import com.example.praktikum5.ui.theme.PendaftaranApp
 import com.example.praktikum5.ui.theme.Praktikum5Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() // status bar transparan
         setContent {
             Praktikum5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    // bikin background Scaffold luar jadi transparan
+                    containerColor = Color.Transparent
+                ) { innerPadding ->
+                    // Panggil PendaftaranApp (yang punya Scaffold DALAM)
+                    // dan kasi padding-nya
+                    PendaftaranApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Praktikum5Theme {
-        Greeting("Android")
     }
 }
